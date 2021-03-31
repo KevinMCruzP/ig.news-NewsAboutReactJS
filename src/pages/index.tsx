d177407cc4e665065f595e7ab253c4cbda57b221
Kevin Cruz <kevin_mcp@outlook.com>
2021-03-30 22:34:07 -0300

    html, scss config done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
+import Head from 'next/head';
 
 export default function Home() {
   return (
-   <h1>Hello Word!</h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>
+        Hello Word!
+      </h1>
+    </>
   )
 }
